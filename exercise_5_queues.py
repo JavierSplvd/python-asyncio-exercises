3b4c3c739b3b6657573f4ab602bfd25b35030629
numian <takatsho@gmail.com>
2025-08-31 13:40:25 +0200

    feat: refactor queue

--- exercise_5_queues.py
+++ exercise_5_queues.py
@@ -1,5 +1,11 @@
 """
 - Queues: Asynchronous FIFO queues for producer-consumer patterns.
+
+In this exercise we combine different concepts. We will create a queue with the id of the workers. Each worker will sleep for a random amount of time and then finish.
+
+We use `create_task()` to create a task for each worker. We use `get()` to get the id of the worker and `task_done()` to signal that the worker is done.
+
+The `join()` method is used to wait for all the workers to finish, they will finish when the queue is empty.
 """
 
 import asyncio
@@ -7,25 +13,24 @@ import random
 
 queue = asyncio.Queue(maxsize=3)
 
-async def worker_sleep(index):
+async def random_sleep_coroutine(index):
     random_time = random.randint(1, 5)
     await asyncio.sleep(random_time)
-    print(f'Worker {index}, {random_time} seconds')
+    print(f'Worker #{index}, {random_time} seconds')
 
 async def worker():
     while True:
-        task = await queue.get()
+        worker_index = await queue.get()
         try:
-            await task
+            await random_sleep_coroutine(worker_index)
         finally:
             queue.task_done()
 
 async def main():
     workers = [asyncio.create_task(worker()) for _ in range(3)]
-    
-    for i in range(3):
-        task = worker_sleep(i)
-        await queue.put(task)
+
+    for worker_id in range(3):
+        await queue.put(worker_id)
 
     await queue.join()
 
